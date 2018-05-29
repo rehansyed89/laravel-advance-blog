6d989919a5cd8cef8daa3acb6f36b0593fdb0249
rmazhar <rmazhar-ext@cci-paris-idf.fr>
2018-05-29 10:29:30 +0200

    FO: add images to posts and change color of post title

--- resources/views/user/post/partials/header.blade.php
+++ resources/views/user/post/partials/header.blade.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="post-heading">
+                <div class="post-heading" style="color: #070707;">
                     <h1>@yield('blog_header_title')</h1>
                     <h2 class="subheading">@yield('blog_header_subtitle')</h2>
-                    <span class="meta">Posted by <a href="#">Rehan Mazhar</a> on May 5, 2018</span>
+                    <span class="meta">Posted by <a href="#" style="color: #070707;">Rehan Mazhar</a> on May 5, 2018</span>
                 </div>
             </div>
         </div>
